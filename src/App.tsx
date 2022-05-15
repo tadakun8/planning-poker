05857c1bc4825c07d4a3d9ed3787a5b62d87a3f7
tadakun8 <tadakun8.lab@gmail.com>
2022-05-15 14:28:19 +0900

    style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Box } from '@chakra-ui/react';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Box } from "@chakra-ui/react";
 
 function App() {
   return (
