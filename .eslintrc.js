19194fab229e55a02a7df69e112dc3dc6a2e365f
tadakun8 <tadakun8.lab@gmail.com>
2022-05-15 16:36:59 +0900

    fix: Replace react

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:storybook/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
---
+++ src/customHooks/useFetchProjectList.ts
@@ -0,0 +1,33 @@
+import { collection, CollectionReference, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import db from "../firebase";
+import { Project } from "../types/model";
+
+/**
+ * Fetch a list of projects registered in the DB
+ * @returns list of projects
+ */
+export const useFetchProjectList = () => {
+  const [projectList, setProjectList] = useState<Project[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      // Fetch snapshot of all project data in DB
+      const querySnapshot = await getDocs(
+        collection(db, "projects") as CollectionReference<Project>
+      );
+
+      // Define temporary variables to store project data
+      const _projectList: Project[] = [];
+
+      // Retrieve project data from snapshot
+      querySnapshot.forEach((doc) => _projectList.push(doc.data()));
+
+      // Stores project data
+      setProjectList(_projectList);
+    }
+    fetchData();
+  }, []);
+
+  return projectList;
+};
