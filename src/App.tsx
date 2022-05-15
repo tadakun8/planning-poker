d510dd90f0005d3044915791aea9b3e4cc17be3f
tadakun8 <tadakun8.lab@gmail.com>
2022-05-15 14:09:00 +0900

    chakrauiインストール

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Box } from '@chakra-ui/react';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Box>Hello World</Box>
       </header>
     </div>
   );
